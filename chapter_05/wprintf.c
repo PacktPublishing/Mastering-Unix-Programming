36338c5c32d3fba8b4ee25d6db00cee47319bab3
Rodolfo Giometti <giometti@enneenne.com>
2020-02-04 16:11:12 +0100

    Add chapters
    Signed-off-by: Rodolfo Giometti <giometti@enneenne.com>

---
+++ chapter_05/wprintf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <wchar.h>
+
+int main(void)
+{
+	int weekday = 3, month = 10, day = 27, year = 1970,
+	    hour = 22, min = 30
+	wprintf(L"%s, %d %s %d, %d:%.2d\n",
+		weekday, year, );
+
+	return 0;
+}
